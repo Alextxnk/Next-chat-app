458e5a5924693654fd20534c4ef4bf6730350d7e
Alextxnk <alexsolov766@gmail.com>
2023-06-07 19:49:07 +0300

    add new files

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -31,8 +31,9 @@ const Input: React.FC<InputProps> = ({
          disabled={disabled}
          {...register(id, { required })}
          className={clsx(
-            'flex h-10 w-full rounded-md border border-slate-300 bg-transparent py-2 px-3 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-700 dark:text-slate-50 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900',
-            errors[id] && 'focus:ring-rose-500',
+            /* 'flex h-10 w-full rounded-md border border-slate-300 bg-transparent py-2 px-3 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-700 dark:text-slate-50 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900',
+            errors[id] && 'focus:ring-rose-500', */
+            'form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6',
             disabled && 'opacity-50 cursor-default'
          )}
       />
